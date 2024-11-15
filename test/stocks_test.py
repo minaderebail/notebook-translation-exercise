6efee8255628c5c47689053d5b7f38314b08b6c5
minaderebail <mad403@georgetown.edu>
2024-11-15 00:04:38 -0500

    Refactor and Test USD Formatter Function

--- test/stocks_test.py
+++ test/stocks_test.py
@@ -2,7 +2,12 @@
 
 from pandas import DataFrame
 
-from app.stocks_report import fetch_stocks_csv
+from app.stocks_report import fetch_stocks_csv, format_usd
+
+def test_usd_formatting():
+    assert format_usd(3.5) == "$3.50"
+    assert format_usd(0.44444) == "$0.44"
+    assert format_usd(123456789) == "$123,456,789.00"
 
 def test_data_fetching():
 
@@ -10,7 +15,7 @@ def test_data_fetching():
     assert isinstance(df, DataFrame)
     assert df.columns.tolist() == ["timestamp", "open", "high", "low", "close", "adjusted_close", "volume", "dividend_amount", "split_coefficient"]
     assert len(df) > 1500
-    
+
     earliest = df.iloc[-1]
     assert earliest["timestamp"] == '2018-04-03'
     assert earliest["adjusted_close"] == 149.01
