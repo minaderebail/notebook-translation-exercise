e46245ef49393d80d2d2b3f11b6616e48ef97b5b
minaderebail <111998382+minaderebail@users.noreply.github.com>
2024-11-14 22:27:23 -0500

    Refactor and Test Reports (#3)

--- app/unemployment_report.py
+++ app/unemployment_report.py
@@ -1,61 +1,87 @@
-# this is the app/unemployment.py file...
+# this is the app/unemployment_report.py file...
 
 # LOCAL DEV (ENV VARS)
 
-import json
-from pprint import pprint
+
 from statistics import mean
+
 
 import requests
 from plotly.express import line
 
+
 from app.alpha import API_KEY
 
-# UNEMPLOYMENT REPORT FUNCTIONALITY
+
+def fetch_unemployment_json():
+
+    request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}"
+
+    response = requests.get(request_url)
+
+    parsed_response = response.json()
+
+    #return parsed_response["data"]
+
+    # we probably want to clean the data before returning it
+    # including converting string values to floats
+    data = parsed_response["data"]
+
+    # we could use a traditional mapping approach and collect the data into a new empty list
+    # but here we are mutating / changing the data in place. choose whatever approach you like
+
+    #clean_data = []
+    #for item in data:
+    #    clean_data.append({"date": item["date"], "value": float(value)})
+    #return clean_data
+
+    for item in data:
+        item["value"] = float(item["value"])
+
+    return data
 
 
-request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}"
 
-response = requests.get(request_url)
-
-parsed_response = json.loads(response.text)
-print(type(parsed_response))
-pprint(parsed_response)
-
-data = parsed_response["data"]
-
-# Challenge A
-#
-# What is the most recent unemployment rate? And the corresponding date?
-# Display the unemployment rate using a percent sign.
-
-print("-------------------------")
-print("LATEST UNEMPLOYMENT RATE:")
-#print(data[0])
-print(f"{data[0]['value']}%", "as of", data[0]["date"])
-
-# Challenge B
-#
-# What is the average unemployment rate for all months during this calendar year?
-# ... How many months does this cover?
+if __name__ == "__main__":
 
 
-this_year = [d for d in data if "2022-" in d["date"]]
+    data = fetch_unemployment_json()
 
-rates_this_year = [float(d["value"]) for d in this_year]
-#print(rates_this_year)
+    # Challenge A
+    #
+    # What is the most recent unemployment rate? And the corresponding date?
+    # Display the unemployment rate using a percent sign.
 
-print("-------------------------")
-print("AVG UNEMPLOYMENT THIS YEAR:", f"{mean(rates_this_year)}%")
-print("NO MONTHS:", len(this_year))
-
-# Challenge C
-#
-# Plot a line chart of unemployment rates over time.
+    print("-------------------------")
+    print("LATEST UNEMPLOYMENT RATE:")
+    #print(data[0])
+    print(f"{data[0]['value']}%", "as of", data[0]["date"])
 
 
-dates = [d["date"] for d in data]
-rates = [float(d["value"]) for d in data]
 
-fig = line(x=dates, y=rates, title="United States Unemployment Rate over time", labels= {"x": "Month", "y": "Unemployment Rate"})
-fig.show()
+
+    # Challenge B
+    #
+    # What is the average unemployment rate for all months during this calendar year?
+    # ... How many months does this cover?
+
+
+    this_year = [d for d in data if "2022-" in d["date"]]
+
+    rates_this_year = [float(d["value"]) for d in this_year]
+    #print(rates_this_year)
+
+    print("-------------------------")
+    print("AVG UNEMPLOYMENT THIS YEAR:", f"{mean(rates_this_year)}%")
+    print("NO MONTHS:", len(this_year))
+
+
+    # Challenge C
+    #
+    # Plot a line chart of unemployment rates over time.
+
+    dates = [d["date"] for d in data]
+    rates = [float(d["value"]) for d in data]
+
+    fig = line(x=dates, y=rates, title="United States Unemployment Rate over time", labels= {"x": "Month", "y": "Unemployment Rate"})
+    fig.show()
